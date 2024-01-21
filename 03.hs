0f0ab7d621d4c77c2303a622da9512ef0eb84b06
emiruz <hi@emiruz.com>
2024-01-21 21:43:27 +0100

    Day 03.

--- 03.hs
+++ 03.hs
@@ -7,10 +7,10 @@ priority :: Char -> Int
 priority x = maybe 0 (\i -> [1..] !! i) $ elemIndex x xs
   where xs = ['a'..'z'] ++ ['A'..'Z']
 
-pick :: [String] -> [Int]
-pick (x:y:z:xs) =
-  (priority . head $ intersect x $ intersect y z) : pick xs
-pick _ = []
+pick3 :: [String] -> [Int]
+pick3 (x:y:z:xs) =
+  (priority . head $ intersect x $ intersect y z) : pick3 xs
+pick3 _ = []
 
 splitIntersect :: String -> Int
 splitIntersect xs =
@@ -21,5 +21,5 @@ main = do
   content <- getContents -- readFile "input.txt"
   let ls = lines content
       s1 = sum $ map splitIntersect ls
-      s2 = sum $ pick ls
+      s2 = sum $ pick3 ls
   printf "Part 1: %d, Part 2: %d\n" s1 s2
