24f9cd472e43e369a1dc149a58a5ade811e240b0
emiruz <hi@emiruz.com>
2024-01-21 20:31:36 +0100

    Day 02.

---
+++ 02.hs
@@ -0,0 +1,21 @@
+#!/usr/bin/env runhaskell
+
+import Data.List(elemIndex)
+import Text.Printf(printf)
+
+score :: String -> Int
+score x = maybe 0 (+1) $ elemIndex x xs
+  where xs = ["BX","CY","AZ","AX","BY","CZ","CX","AY","BZ"]
+
+pick :: String -> String
+pick x = maybe x (\i -> ys !! i) $ elemIndex x xs
+  where xs = ["AX","AY","AZ","CX","CY","CZ"]
+        ys = ["AZ","AX","AY","CY","CZ","CX"]
+
+main :: IO ()
+main = do
+  content <- getContents --readFile "input.txt"
+  let ls = map (filter (/=' ')) $ lines content
+  let s1 = sum $ map score ls
+  let s2 = sum $ map (score . pick) ls
+  printf "Part 1: %d, Part 2: %d\n" s1 s2
