87a2dacfaffd06d2d3042f401245cee6e42fbbf5
emiruz <hi@emiruz.com>
2024-01-27 17:57:47 +0100

    3x performance improvement.

--- 12.hs
+++ 12.hs
@@ -17,20 +17,18 @@ main = do
       val x | x=='S' = -(ord 'a') | x=='E' = -(ord 'z') | otherwise = -(ord x)
 
       q g f (x,y)  = maybe 1000 f $ M.lookup ((x-1)*n+y) g
-      q' g i = maybe 1000 id $ M.lookup i g
 
-      adj (x,y) = filter (\p->v0-1 <= q grid val p) opts
-        where v0 = q grid val (x,y)
+      adj (x,y)    = filter (\p->v0-1 <= q grid val p) opts
+        where v0   = q grid val (x,y)
               opts = map (\(a,b)->(x+a,b+y)) [(1,0),(-1,0),(0,1),(0,-1)]
 
-      adj' = zip [1..] (map adj ps)
-  
+      adj'  = zip [1..] (map adj ps)
       start = M.insert (1+ei 'E') 0 $ M.fromList $ zip [1..] $ replicate (n*m) 1000
 
       paths vs | vs' == vs = vs | otherwise = paths vs'
-        where vs' = foldl' (\acc (i,a)-> upd acc i $ ff (i,a)) vs adj'
-              upd acc i (v0,v) = if v /= v0 then M.insert i v acc else acc
-              ff (i,a) = let v0 = q' vs i in (v0, minimum $ v0:(map ((+1).(q vs id)) a))
+        where vs' = foldl' (\acc y@(v0,(i,_))-> upd acc i (ff y) v0) vs (zip (M.elems vs) adj')
+              upd acc i v v0 = if v /= v0 then M.insert i v acc else acc
+              ff (v0,(_,a)) = minimum $ v0:(map ((+1).(q vs id)) a)
 
       solve = paths start
       part1 = maybe 0 id $ M.lookup (1+ei 'S') solve
