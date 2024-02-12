0b7defae9d9616443bff94181f1d48b16609f0b2
emiruz <hi@emiruz.com>
2024-02-12 15:11:40 +0100

    10x time improvement.

--- 19.hs
+++ 19.hs
@@ -10,7 +10,7 @@ parse str = bp $ map (\x->read x :: Int) strs
         bp (_:a:b:c:d:e:f:xs) = (a,b,c,d,e,f) : bp xs; bp _ = []
 
 opt :: Integral b => b -> M.Map (b, [b]) b -> [b] -> b -> (b, b, b, b, b, b) -> (M.Map (b, [b]) b, b)
-opt mx cache state 0 _ = (cache, last $ take 4 state)
+opt _ cache state 0 _ = (cache, last $ take 4 state)
 opt mx cache state@[i,j,k,l,m,n,o,p] bu bp@(a,b,c,d,e,f) = case M.lookup (bu,state) cache of
   Just score' -> (cache, score')
   Nothing -> (M.insert (bu,state) val cache', val)
@@ -18,16 +18,15 @@ opt mx cache state@[i,j,k,l,m,n,o,p] bu bp@(a,b,c,d,e,f) = case M.lookup (bu,sta
         (_,cache',next) = foldl (\(mx',ca,vs) x->let (c',v)=opt mx' ca x (bu-1) bp in (max mx' v, c',v:vs)) (mx,cache,[]) next'
         next' = map (zipWith (+) [m,n,o,p,0,0,0,0]) $  next''
         next'' | i>=e && k>=f = [geo] | i>=c && j>=d = [obs] | otherwise = filter (all (>=0)) $ next'''
-        next''' = if (est k o bu) > mx && cond then [nul,ore,cla] else [nul]
-        est _ _ 0 = 0
-        est o o' b = (if o+o'>=f then 1*(b-1) else 0) + est (mod (o+o') f) (o'+1) (b-1)
+        next''' = if (est k o bu-1) > mx && cond then [cla,ore,nul] else [nul]
+        est o o' b | b<=0 = 0 | otherwise = (if o+o'>=f then 1*(b-1) else 0) + est (mod (o+o') f) (o'+1) (b-1)
         cond  = any (==0) [mod i a, mod i b, mod i c, mod i e, mod j d, mod k f]
         nul   = [i,   j,   k,   l, m,   n,   o,   p]
         ore   = [i-a, j,   k,   l, m+1, n,   o,   p]
         cla   = [i-b, j,   k,   l, m,   n+1, o,   p]
         obs   = [i-c, j-d, k,   l, m,   n,   o+1, p]
         geo   = [i-e, j,   k-f, l, m,   n,   o,   p+1]
-opt _ _ _ _ _ = (M.empty,0)
+opt _ _ _ _ _ = (M.empty, 0)
 
 main :: IO ()
 main = do
