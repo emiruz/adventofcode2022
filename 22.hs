08bad005d34200a5ebd16ecb2078e28b1c4aeba6
emiruz <hi@emiruz.com>
2024-02-18 09:11:56 +0100

    Small refactor of part1

--- 22.hs
+++ 22.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env runhaskell
-{-# OPTIONS_GHC -Wall #-}
+
 import Text.Regex.PCRE
 import Data.List(elemIndex)
 import Text.Printf(printf)
@@ -10,34 +10,33 @@ type Coord = (Int, Int)
 type Grid = S.Set (Int, Int, Char)
 
 walk :: Grid -> String -> Coord -> Coord -> (Int, Coord)
-walk coo (x:xs) o l
-  | elem x "LR"            = walk coo xs ([(0,1),(1,0),(0,-1),(-1,0)] !! i') l
-  | S.member (i,j,'.') coo = walk coo xs o (i,j)
-  | otherwise              = walk coo xs o l
-  where (i,j) = wrap coo o l
-        i'    = (oIndex o + (if x=='R' then 1 else -1)) `mod` 4
-walk _ [] o l = (oIndex o, l)
-
-wrap :: Grid -> Coord -> Coord -> Coord
-wrap coo (i,j) (a,b)
-  | any (\x-> S.member (a+i,b+j,x) coo) ".#" = (a+i,b+j)
-  | otherwise = [(a,minimum col),(minimum row,b),(a,maximum col),(maximum row,b)] !! oIndex (i,j)
-  where row = [x | (x,y,_) <- S.toList coo, y == b]
+walk coo (x':xs) o@(i,j) l@(a,b)
+  | elem x' "LR"             = walk coo xs ([(0,1),(1,0),(0,-1),(-1,0)] !! idx) l
+  | S.member (i',j','.') coo = walk coo xs o (i',j')
+  | otherwise                = walk coo xs o l
+  where (i',j') = wrap
+        idx = (oIndex o + (if x'=='R' then 1 else -1)) `mod` 4
+        row = [x | (x,y,_) <- S.toList coo, y == b]
         col = [y | (x,y,_) <- S.toList coo, x == a]
+        wrap | any (\x-> S.member (a+i,b+j,x) coo) ".#" = (a+i,b+j)
+             | otherwise = [(a,minimum col),(minimum row,b),(a,maximum col),(maximum row,b)] !! oIndex (i,j)
+walk _ [] o l = (oIndex o, l)
 
 oIndex :: Coord -> Int
 oIndex o = fromJust $ elemIndex o os where os = [(0,1),(1,0),(0,-1),(-1,0)]
 
+parse :: String -> ([(Int, Int, Char)], [Char])
+parse str = (coo, ins)
+  where (str1,_,str2) = str =~ "(?s)\n\n" :: (String, String, String)
+        ls    = lines str1
+        coo   = filter (\(_,_,v) -> v /= ' ') $ [(i,j,v) | (i,l)<-zip [1..] ls, (j,v)<-zip [1..length l] l]
+        ins   = concat $ map (\x->if x=~"[0-9]+" then replicate (read x) '.' else x) $ getAllTextMatches $ str2 =~ "L|R|[0-9]+"
+
 main :: IO ()
 main = do
   str <- getContents --readFile "input.txt"
-  let (str1,_,str2) = str =~ "(?s)\n\n" :: (String, String, String)
-      ls    = lines str1
-      coo   = filter (\(_,_,v) -> v /= ' ') $
-        [(i,j,v) | (i,l)<-zip [1..] ls, (j,v)<-zip [1..length l] l]
-      ins   = concat $ map (\x->if x=~"[0-9]+" then replicate (read x) '.' else x) $
-        getAllTextMatches $ str2 =~ "L|R|[0-9]+"
-        
-      (face,(a',b')) = walk (S.fromList coo) ins (0,1) ((\(a,b,_)->(a,b)) $ head coo)
-      part1 = 1000 * a' + 4 * b' + face
+  let (coo, ins)     = parse str
+      first          = ((\(a,b,_)->(a,b)) $ head coo)
+      (face,(a',b')) = walk (S.fromList coo) ins (0,1) first
+      part1          = 1000 * a' + 4 * b' + face
   printf "Part 1: %d, Part 2: TBD\n" part1
